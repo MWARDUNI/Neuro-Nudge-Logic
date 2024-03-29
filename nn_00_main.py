be1628942bb42bb62adb21ca5f324c1012387a51
Matt Ward <89886168+MWARDUNI@users.noreply.github.com>
2024-03-29 11:01:59 -0600

    Update nn_00_main.py

--- nn_00_main.py
+++ nn_00_main.py
@@ -19,16 +19,28 @@ def main():
     # nn_02_categorizer.py
     categorized_assignments = categorize_assignments(assignments)
 
-    # Impacts on final grade for each assignment type
+    # Impacts on final grade for TOTAL assignment type
     grading_info = {
         # Theory
-        'CSCI 4034': {'homework': 20, 'project': 30, 'final': 50},
+        'CSCI 4034': {'homework': 33, 'midterm': 33, 'final': 33}, # 3 homeworks 11% each
         # Network Programming
-        'CSCI 3762': {'homework': 15, 'project': 35, 'final': 50},
-        # Security
-        'CSCI 4743': {'lab': 25, 'project': 35, 'final': 40},
+        'CSCI 3762': {'lab': 100}, # 10 labs 10% each
         # Embedded Systems
-        'CSCI 4287': {'homework': 20, 'project': 30, 'final': 50}
+        'CSCI 4287': {'homework': 30, 'quiz': 30, 'final': 40}, # 5 homeworks 6% each, 3 quizzes 10% each, final 40%
+        # Security
+        'CSCI 4743': {'lab': 25, 'project': 35, 'final': 40} # 
+    }
+
+    # Impacts on final grade for EACH assignment type
+    grade_impact_individually = {
+        # Theory
+        'CSCI 4034': {'homework': 11, 'midterm': 33, 'final': 33}, # 3 homeworks 11% each
+        # Network Programming
+        'CSCI 3762': {'lab': 10}, # 10 labs 10% each (NEED TO ADJUST THE CALENDAR FILE)
+        # Security
+        'CSCI 4743': {'homework': 6, 'quiz': 10, 'midterm': 20, 'final': 20}, # 5 homeworks 6% each, 3 quizzes 10% each
+        # Embedded Systems
+        'CSCI 4287': {'labs': 5, 'midterm': 20, 'final': 20, 'project': 40} # 4 labs 5% each, midterm 20%, final 20%, project 40%
     }
 
     # nn_03_prioritizer.py
