10ca77bd2cd464e093ec19c3467fc96100b69db6
Matt Ward <89886168+MWARDUNI@users.noreply.github.com>
2024-03-29 11:01:11 -0600

    Update nn_02_categorizer.py

--- nn_02_categorizer.py
+++ nn_02_categorizer.py
@@ -52,12 +52,24 @@ assignments = [
     {'due_date': '2023-05-15', 'type': 'final', 'class': 'CSCI 4287', 'description': 'Embedded Systems Design'},
 ]
 
-
+# TODO: logic for dividing up multiple assignments for proper impact
 grading_info = {
     'CSCI 4034': {'homework': 20, 'project': 30, 'final': 50},
     'CSCI 3762': {'homework': 15, 'project': 35, 'final': 50},
     'CSCI 4743': {'lab': 25, 'project': 35, 'final': 40},
     'CSCI 4287': {'homework': 20, 'project': 30, 'final': 50}
+}
+
+# Impacts on final grade for EACH assignment type
+grade_impact_individually = {
+    # Theory
+    'CSCI 4034': {'homework': 11, 'midterm': 33, 'final': 33}, # 3 homeworks 11% each
+    # Network Programming
+    'CSCI 3762': {'lab': 10}, # 10 labs 10% each (NEED TO ADJUST THE CALENDAR FILE)
+    # Security
+    'CSCI 4743': {'homework': 6, 'quiz': 10, 'midterm': 20, 'final': 20}, # 5 homeworks 6% each, 3 quizzes 10% each
+    # Embedded Systems
+    'CSCI 4287': {'labs': 5, 'midterm': 20, 'final': 20, 'project': 40} # 4 labs 5% each, midterm 20%, final 20%, project 40%
 }
 
 categorized = categorize_assignments(assignments)
