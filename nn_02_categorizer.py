cb3ea7a8c70f4be2351379bb16c6cd0879fcb11e
Matt Ward <89886168+MWARDUNI@users.noreply.github.com>
2024-03-29 11:02:55 -0600

    Update nn_02_categorizer.py

--- nn_02_categorizer.py
+++ nn_02_categorizer.py
@@ -53,11 +53,16 @@ assignments = [
 ]
 
 # TODO: logic for dividing up multiple assignments for proper impact
+# Impacts on final grade for TOTAL assignment type
 grading_info = {
-    'CSCI 4034': {'homework': 20, 'project': 30, 'final': 50},
-    'CSCI 3762': {'homework': 15, 'project': 35, 'final': 50},
-    'CSCI 4743': {'lab': 25, 'project': 35, 'final': 40},
-    'CSCI 4287': {'homework': 20, 'project': 30, 'final': 50}
+    # Theory
+    'CSCI 4034': {'homework': 33, 'midterm': 33, 'final': 33}, # 3 homeworks 11% each
+    # Network Programming
+    'CSCI 3762': {'lab': 100}, # 10 labs 10% each
+    # Embedded Systems
+    'CSCI 4287': {'homework': 30, 'quiz': 30, 'final': 40}, # 5 homeworks 6% each, 3 quizzes 10% each, final 40%
+    # Security
+    'CSCI 4743': {'lab': 25, 'project': 35, 'final': 40} # 
 }
 
 # Impacts on final grade for EACH assignment type
