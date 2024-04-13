9c7ca6c33ce69808559cbb143c3393ceccc22633
Brian Hagerty <hagerty@sawatchlabs.com>
2024-04-13 11:24:02 -0600

    Small update

--- fill_blocks.py
+++ fill_blocks.py
@@ -2,29 +2,13 @@
 
 import sys, datetime, psycopg2
 from supabase import create_client, Client
+from utils import unpack_cursor
 
 OPEN_BLOCKS_DAILY = 8
 NUM_OF_CHUNKS = 3
 
 conn_str = "user=postgres.fgocfoakntmlhgtftrzh password=RkTM2EYlp93mnld6 host=aws-0-us-west-1.pooler.supabase.com port=5432 dbname=postgres"
 
-def unpack_cursor(cur):
-    res = cur.fetchall()
-    if len(res) == 1:
-        if len(res[0]) == 1:
-            return res[0][0]
-        else:
-            return list(res[0])
-        
-    else:
-        output = []
-        for r in res:
-            if len(r) == 1:
-                output.append(r[0])
-            else:
-                output.append(list(r))
-        
-        return output
 
 class BlockFiller():
     def __init__(self, supabase: Client, start_date: datetime, end_date: datetime) -> None:
@@ -33,27 +17,25 @@ class BlockFiller():
         self.end_date = end_date
         self.conn = psycopg2.connect(conn_str)
     
-    def main(self, **kwargs):
-        # kwargs pattern just for testing
+    def main(self, fill_dict: dict):
+        # Can turn on/off fill flags for testing
         
         # 0) Populate semester?
-        if "semester" in kwargs:
-            self.populate_semester()
+        if fill_dict['semester']:
+            # self.populate_semester()
+            self.populate_sched_days()
         
         # 1) Populate classes/events
-        if "events" in kwargs:
+        if fill_dict['events']:
             ...
         
         # 2) 5 days study plans
-        if "tests" in kwargs:
+        if fill_dict['tests']:
             ...
         
         # 3) fill in assignments
-        if "assignments" in kwargs:
+        if fill_dict['assignments']:
             self.populate_assignments()
-        
-        
-        
         
         
         if self.conn is not None:
@@ -67,75 +49,79 @@ class BlockFiller():
             dates.append({"local_date": current, "open_blocks": OPEN_BLOCKS_DAILY})
             current = current + datetime.timedelta(days=1)
             
+        # need to populate blocked off times
+            
         return None
+    
+    def populate_sched_days(self):
+        cur = self.conn.cursor()
+        current = self.start_date
+        while current <= self.end_date:
+            for i in range(1, 25): # do we want 0 to 23 instead?
+                sql = f"insert into sched_days(local_date, local_hour, blocked) values ('{current}', {i}, false)"
+                cur.execute(sql)
+                current = current + datetime.timedelta(days=1)
+        cur.close()
     
     def populate_assignments(self):
         # Get semester objects by date desc
-        # days = self.supabase.table("semester").select("*").eq("filled", False).execute()
         
         cur = self.conn.cursor()
         
-        # Maybe we just don't need to actually pull them?
-        sql = f"select local_date, open_blocks from semester where open_blocks > 0 order by local_date desc"
-        cur.execute(sql)
-        dates = unpack_cursor(cur)
-        # myDict = { k:v for (k,v) in zip(keys, values)}
-        dates = {k:v for k,v in dates}
-        # COULD STORE SEMESTER BLOCKS AS OFFSET FROM START (or back?)
-        # what about a list of these days
-
-        
         
         # Get all assignments by due date desc
-        sql = f"select due, description from assignments where type = 'hw' or type = 'lab' order by due_date desc"
+        # Extract hour for ordering purposes (?)
+        sql = f"select EXTRACT(DAY FROM due) as day, description, uid, EXTRACT(HOUR FROM due) as hour from assignments where \
+            type = 'hw' or type = 'lab' order by hour asc, day desc"
         cur.execute(sql)
         ass = unpack_cursor(cur)
         
         for a in ass:
             # change these to line up
-            ass_date = a['due_date']
-            description = a['des']
-            id = a['id']
+            ass_date = a[0]
+            description = a[1]
+            id = a[2]
             
             # Break assignments into chucks/blocks
-            for i in range(1, NUM_OF_CHUNKS, -1):
+            for i in range(NUM_OF_CHUNKS, 0, -1):
                 sql = f"insert into chunks(assignment_id, due_date, chunk_no, title) values('{id}', '{ass_date}', {i}, '{description}')"
-                # chunk = f"{i}"
+                
                 ass_date = ass_date - datetime.timedelta(days=2)
             
                 
                 
         
-        sql = f"select * from chunks order by due_date desc" #might switch order?
+        sql = f"select assignment_id, due_date, chunk_no from chunks order by due_date desc" #might switch order?
         
         # Go through chunks by due date desc and then block num desc
         # Assign chunk to alternating days (or just lower block number?)
-        #COULD MARK CHUNK DUE DATE AS 2 DAYS BACK
         chunks = []
         for chunk in chunks:
-            # Grab dates and then grab chunks that are due that date???
-            sql = f"update chunk set start_time = (select min(local_hour) from sched_days where blocked is false and local_date = {chunk['due_date']}), \
+            sql = f"update chunk set start_time = (select coalesce(min(local_hour) from sched_days where blocked is false and local_date = '{chunk[1]}'), \
+                min(local_hour) from sched_days where blocked is false and \
+                    local_date = (select max(local_date) from sched_days where blocked is false and local_date <= '{chunk[1]}') ), \
                 stop_time = (start_time + interval '1 hour') \
-                where assignment_id = '{chunk['id']}' and chunk_no = {chunk['chunk_no']}"
-            
+                where assignment_id = '{chunk[0]}' and chunk_no = {chunk[2]}"
+                
+            # Also update sched_days to be blocked
+            sql = f"update sched_days set blocked = true where \
+                local_date = (select due_date from chunks where assignment_id = '{chunk[0]}' and chunk_no = {chunk[2]}) \
+                    and local_time = (select start_time from chunks where assignment_id = '{chunk[0]}' and chunk_no = {chunk[2]})"
         
-        start = self.start_date
-        current = self.end_date
-        while current >= start:
-            sql = f"select * from chunks where due_date = '{current}' order by chunk_no desc"
+        # start = self.start_date
+        # current = self.end_date
+        # while current >= start:
+        #     sql = f"select * from chunks where due_date = '{current}' order by chunk_no desc"
             
-            sql = f"select "
+        #     sql = f"select "
             
-            current = current - datetime.timedelta(days=1)
+        #     current = current - datetime.timedelta(days=1)
         
         
         
         # after assignment, update semester blocks??
         cur.close()
         
-    # def build_chunks(self, assignments):
-    #     for assignment in assignments:
-    #         ...
             
 
 if __name__ == "__main__":
@@ -146,5 +132,7 @@ if __name__ == "__main__":
     
     supabase: Client = create_client(url, key)
     
+    to_fill = {"semester":False, "tests":False, "events":False, "assignments":False}
+    
     bf = BlockFiller(supabase)
-    bf.main()
+    bf.main(to_fill)
